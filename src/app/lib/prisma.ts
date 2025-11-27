22305968d9afd33fb6a8be1ce8c96ff219e892d8
wakato <itlogin0606@gmail.com>
2025-11-27 13:02:54 +0900

    しゅうせいできたらいいな～～～

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,8 +1,9 @@
-// src/lib/prisma.ts
-
 import { PrismaClient } from '@prisma/client';
 
-// PrismaClientのインスタンスを作成します
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
